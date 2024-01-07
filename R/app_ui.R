eb293541bd48a00b0e09b447aafd3894ca6e2812
arnfinn <arnfinn.steindal@gmail.com>
2024-01-07 18:02:08 +0100

    å

--- R/app_ui.R
+++ R/app_ui.R
@@ -101,7 +101,7 @@ app_ui <- function() {
       ),
       shiny::tabPanel(
         shiny::span("Abonnement v2",
-                    title="Bestill tilsending av rapporter på e-post"),
+                    title="Bestill tilsending av rapporter p\u00e5 e-post"),
         shiny::sidebarLayout(
           shiny::sidebarPanel(
             rapbase::autoReportInput("testSubscription")
