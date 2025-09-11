fbacbd0a0f811416abd98a4f793f5d60c73e48e2
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2025-09-11 16:17:49 +0200

    Slettet ubrukte elementer fra server-fil
    Henger igjen fra gammel måte å vise brukerinformasjon på

--- R/app_server.R
+++ R/app_server.R
@@ -13,26 +13,6 @@ app_server <- function(input, output, session) {
     orgName = "pilot"
   )
 
-  # Brukerinformasjon i menylinja (navbar)
-  output$appUserName <- shiny::renderText(
-    paste(
-      rapbase::getUserFullName(session),
-      rapbase::getUserRole(session), sep = ", "
-    )
-  )
-  output$appOrgName <- shiny::renderText(rapbase::getUserReshId(session))
-  userInfo <-
-    rapbase::howWeDealWithPersonalData(session, callerPkg = "rapRegTemplate")
-
-  shiny::observeEvent(input$userInfo, {
-    shinyalert::shinyalert(
-      "Dette vet Rapporteket om deg:", userInfo,
-      type = "", imageUrl = "rap/logo.svg",
-      closeOnEsc = TRUE, closeOnClickOutside = TRUE,
-      html = TRUE, confirmButtonText = rapbase::noOptOutOk()
-    )
-  })
-
   veiledning_server("veiledning")
   plots_server("plots")
   samlerapport_server("samlerapport")
