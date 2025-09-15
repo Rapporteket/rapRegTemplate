1ec61d2013075e506ab102cf7ca4fd2a1bbebc14
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2025-09-15 13:56:21 +0200

    update doc

--- R/MakeHist.R
+++ R/MakeHist.R
@@ -3,16 +3,14 @@
 #' Short demo on how to produce dynamic content in a shiny app at Rapporteket
 #'
 #' @param df dataframe from which output is to be made
-#' @param var string defining which varable in the data frame to use
-#' @param bins numeric vector defining the number of equally large groups
+#' @param var string defining which variable in the data frame to use
 #' @param makeTable Logical that if TRUE function will return a data frame
 #' containin the bin borders and count within each bin
+#' @param x What to plot
 #'
 #' @return a graphical object or data frame
 #' @export
 #'
-#' @examples
-#' makeHist(df = mtcars, var = "mpg", bins = 5, makeTable = FALSE)
 
 makeHist <- function(df, x, var, makeTable = FALSE) {
 
