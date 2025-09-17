dd988e805526f138ce081b9bb6ec2b82b313839f
Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>
2025-09-17 11:18:07 +0200

    lintr

--- R/func_over_tid.R
+++ R/func_over_tid.R
@@ -11,30 +11,32 @@
 over_tid_utvalg <- function(data, var, valg_region) {
 
   data <- data %>%
-    dplyr::select(year, region, dplyr::all_of(!!var))
+    dplyr::select(.data$year, .data$region, dplyr::all_of(!!var))
 
   gj_alle <- data %>%
     dplyr::rename(variabelen = {{var}}) %>%
-    dplyr::group_by(year) %>%
-    dplyr::summarize(gjennomsnitt = mean(variabelen)) %>%
-    dplyr::mutate(gjennomsnitt = round(gjennomsnitt, 2)) %>%
-    dplyr::rename(gj_alle = gjennomsnitt)
+    dplyr::group_by(.data$year) %>%
+    dplyr::summarize(gjennomsnitt = mean(.data$variabelen)) %>%
+    dplyr::mutate(gjennomsnitt = round(.data$gjennomsnitt, 2)) %>%
+    dplyr::rename(gj_alle = .data$gjennomsnitt)
 
   data <- data %>%
-    dplyr::filter(region == dplyr::case_when({{valg_region}} == "AFRO" ~ "AFRO",
-                                             {{valg_region}} == "AMRO" ~ "AMRO",
-                                             {{valg_region}} == "EMRO" ~ "EMRO",
-                                             {{valg_region}} == "EURO" ~ "EURO",
-                                             {{valg_region}} == "SEARO" ~ "SEARO",
-                                             {{valg_region}} == "WPRO" ~ "WPRO",
-                                             {{valg_region}} == "Alle" ~ region,
-                                             {{valg_region}} == "Alle_delt" ~ region))
+    dplyr::filter(.data$region == dplyr::case_when(
+      {{valg_region}} == "AFRO" ~ "AFRO",
+      {{valg_region}} == "AMRO" ~ "AMRO",
+      {{valg_region}} == "EMRO" ~ "EMRO",
+      {{valg_region}} == "EURO" ~ "EURO",
+      {{valg_region}} == "SEARO" ~ "SEARO",
+      {{valg_region}} == "WPRO" ~ "WPRO",
+      {{valg_region}} == "Alle" ~ region,
+      {{valg_region}} == "Alle_delt" ~ region
+    ))
 
   gj <- data %>%
     dplyr::rename(variabelen = {{var}}) %>%
-    dplyr::group_by(year, region) %>%
-    dplyr::summarize(gjennomsnitt = mean(variabelen)) %>%
-    dplyr::mutate(gjennomsnitt = round(gjennomsnitt, 2))
+    dplyr::group_by(.data$year, .data$region) %>%
+    dplyr::summarize(gjennomsnitt = mean(.data$variabelen)) %>%
+    dplyr::mutate(gjennomsnitt = round(.data$gjennomsnitt, 2))
 
   gj <- dplyr::left_join(gj, gj_alle)
 
@@ -59,19 +61,22 @@ over_tid_plot <- function(data, valg_region) {
 
   if (valg_region == "Alle_delt") {
     tid_plot <- tid_plot +
-      ggplot2::geom_col(data = data, ggplot2::aes(x = year, y = gjennomsnitt, fill = region)) +
+      ggplot2::geom_col(data = data, ggplot2::aes(x = .data$year, y = .data$gjennomsnitt, fill = .data$region)) +
       ggplot2::scale_fill_brewer(palette = "Set3") +
       ggplot2::labs(fill = "Region")
   } else {
     if (valg_region == "Alle") {
       tid_plot <- tid_plot +
-        ggplot2::geom_col(data = data, ggplot2::aes(x = year, y = gj_alle), fill = "#6CACE4", alpha = .7)
+        ggplot2::geom_col(data = data, ggplot2::aes(x = .data$year, y = .data$gj_alle), fill = "#6CACE4", alpha = .7)
     } else {
       data <- data %>%
         dplyr::mutate(Verden = "Verden (gj.snitt)")
       tid_plot <- tid_plot +
-        ggplot2::geom_col(data = data, ggplot2::aes(x = year, y = gjennomsnitt, fill = region), alpha = .7) +
-        ggplot2::geom_point(data = data, ggplot2::aes(x = year, y = gj_alle, color = Verden)) +
+        ggplot2::geom_col(
+          data = data,
+          ggplot2::aes(x = .data$year, y = .data$gjennomsnitt, fill = .data$region), alpha = .7
+        ) +
+        ggplot2::geom_point(data = data, ggplot2::aes(x = .data$year, y = .data$gj_alle, color = .data$Verden)) +
         ggplot2::scale_color_manual(values = c("Verden (gj.snitt)" = "#003087")) +
         ggplot2::scale_fill_manual(values = c("#6CACE4"))
     }
