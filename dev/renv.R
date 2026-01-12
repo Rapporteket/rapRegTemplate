e7c85b2751467f09e4b7d2ea43abff2ff774ae21
arnfinn <arnfinn.steindal@gmail.com>
2026-01-12 16:09:25 +0100

    mssql ser ut til å fungere med autoreport...

--- dev/renv.R
+++ dev/renv.R
@@ -12,8 +12,11 @@ Sys.setenv(FALK_EXTENDED_USER_RIGHTS = "[
 Sys.setenv(FALK_APP_ID = "80")
 
 Sys.setenv(MYSQL_DB_LOG = "db_log")
-Sys.setenv(MYSQL_DB_AUTOREPORT = "db_autoreport")
+Sys.setenv(MYSQL_DB_AUTOREPORT = "autoreport")
 Sys.setenv(MYSQL_DB_DATA = "deformitet")
-Sys.setenv(MYSQL_HOST = "localhost")
-Sys.setenv(MYSQL_USER = "root")
-Sys.setenv(MYSQL_PASSWORD = "root")
+Sys.setenv(MYSQL_HOST = "a858b665e0bd")
+Sys.setenv(MYSQL_USER = "sa")
+Sys.setenv(MYSQL_PASSWORD = "Your_password123")
+Sys.setenv(DB_TYPE = "mssql")
+Sys.setenv(MYSQL_PORT = 1433)
+
--- dev/run.R
+++ dev/run.R
@@ -3,3 +3,21 @@
 devtools::install("../rapbase", upgrade = FALSE)
 source("dev/renv.R")
 rapRegTemplate::run_app(browser = TRUE)
+
+
+con <- rapbase::rapOpenDbConnection("autoreport")$con
+
+conf <- rapbase:::getDbConfig("autoreport")
+
+con <- DBI::dbConnect(
+  odbc::odbc(),
+  driver = "FreeTDS",
+  database = conf$name,
+  server = conf$host,
+  uid = conf$user,
+  pwd = conf$pass,
+  port = 1433
+)
+
+con <- DBI::dbDisconnect(con)
+con <- NULL
