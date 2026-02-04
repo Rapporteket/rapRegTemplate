b820a8c4e826ed6359a9f94101cb3807d767b4ea
arnfinn <arnfinn.steindal@gmail.com>
2026-02-04 09:45:47 +0100

    arm-fiks i docker-compose

--- dev/run.R
+++ dev/run.R
@@ -1,6 +1,18 @@
 
 
-devtools::install("../rapbase", upgrade = FALSE)
 devtools::install(".", upgrade = FALSE)
+devtools::install("../rapbase", upgrade = FALSE)
 source("dev/renv.R")
 rapRegTemplate::run_app(browser = TRUE)
+
+
+query <- paste0("SELECT * FROM autoreport;")
+res <- rapbase::loadRegData("autoreport", query)
+nms <- names(res) |>
+  setdiff(c("id", "email"))
+
+rest2 <- res |>
+  dplyr::summarise(
+    email = list(unique(email)),
+    .by = nms
+  )
