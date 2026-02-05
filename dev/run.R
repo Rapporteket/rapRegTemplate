8fe6158bc33900628a99b74fdf878dadd65aa754
arnfinn <arnfinn.steindal@gmail.com>
2026-02-05 10:37:24 +0100

    Rydding

--- dev/run.R
+++ dev/run.R
@@ -4,15 +4,3 @@ devtools::install(".", upgrade = FALSE)
 devtools::install("../rapbase", upgrade = FALSE)
 source("dev/renv.R")
 rapRegTemplate::run_app(browser = TRUE)
-
-
-query <- paste0("SELECT * FROM autoreport;")
-res <- rapbase::loadRegData("autoreport", query)
-nms <- names(res) |>
-  setdiff(c("id", "email"))
-
-rest2 <- res |>
-  dplyr::summarise(
-    email = list(unique(email)),
-    .by = nms
-  )
