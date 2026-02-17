aa17f43ecad79e52387428984239d40917cbf2db
petterlinberg <120116699+petterlinberg@users.noreply.github.com>
2026-02-17 10:13:30 +0100

    andeler plot fane (#104)
    Lagt til fane med andeler basert på variabel og grupper
    
    ---------
    
    Co-authored-by: Arnfinn Hykkerud Steindal <arnfinn.hykkerud.steindal@helse-nord.no>

--- R/GetFakeRegData.R
+++ R/GetFakeRegData.R
@@ -9,3 +9,24 @@
 getFakeRegData <- function() {
   cases_year
 }
+
+getFakeRegData2 <- function() {
+  penguin_bool <- palmerpenguins::penguins |>
+    dplyr::mutate(
+      heavy        = .data$body_mass_g >= 4000,
+      long_bill    = .data$bill_length_mm >= 45,
+      deep_bill    = .data$bill_depth_mm >= 18,
+      long_flipper = .data$flipper_length_mm >= 200,
+      male         = .data$sex == "male"
+    )
+  attr(penguin_bool, "kvalIndGrenser") <- list(
+    # fikive kvalitetsindikatorer
+    heavy        = c(0, 30, 60, 100),
+    long_bill    = NA,
+    deep_bill    = NA,
+    long_flipper = c(0, 35, 70, 100),
+    male         = NA
+  )
+  penguin_bool
+
+}
