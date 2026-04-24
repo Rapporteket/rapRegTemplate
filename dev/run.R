fdb37ef7d9a6d928d9d38644564aed8bf4793d22
Arnfinn Hykkerud Steindal <arnfinn.steindal@gmail.com>
2026-04-24 15:56:22 +0200

    Ryddet litt i dev-fila (#127)

--- dev/run.R
+++ dev/run.R
@@ -1,31 +1,6 @@
 
 
-devtools::install("../rapbase", upgrade = FALSE)
-devtools::install(".", upgrade = FALSE)
+devtools::install(".", upgrade = FALSE, dependencies = FALSE)
+devtools::install("../rapbase", upgrade = FALSE, dependencies = FALSE)
 source("dev/renv.R")
 rapRegTemplate::run_app(browser = TRUE)
-
-################
-# MSSQL-greier #
-################
-
-source("dev/renv_mssql.R")
-rapRegTemplate::run_app(browser = TRUE)
-
-con <- rapbase::rapOpenDbConnection("autoreport")$con
-
-conf <- rapbase:::getDbConfig("autoreport")
-
-install.packages("odbc")
-con <- DBI::dbConnect(
-  odbc::odbc(),
-  driver = "FreeTDS",
-  database = conf$name,
-  server = conf$host,
-  uid = conf$user,
-  pwd = conf$pass,
-  port = 1433
-)
-
-con <- DBI::dbDisconnect(con)
-con <- NULL
